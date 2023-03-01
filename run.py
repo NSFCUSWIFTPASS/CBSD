03ce7cc9b7465930d2818226046474da3b7ac4b8
Stefan Tschimben <stefan.tschimben@colorado.edu>
2023-03-01 12:53:38 -0700

    LED update

--- run.py
+++ run.py
@@ -8,7 +8,6 @@ import random
 import sys
 from CBSD import CBSD
 
-# This function can be used to create a random serial number
 def rand_serial(length):
     serial = ''
     for _ in range(length):
@@ -21,19 +20,18 @@ def rand_serial(length):
     return serial
 
 def main():
-    # This application takes one argument in the form of the number of heartbeats until the emulator should relinquish the frequency and deregister
+
     parser = argparse.ArgumentParser()
     required = parser.add_argument_group('required named arguments')
     required.add_argument(dest='heartbeats', metavar='N', type=int, nargs='?', help='Number of Heartbeats to send', default=1)
     args = parser.parse_args()
 
-    # Update these CBSD parameters with your information
-    fcc_id = "your_fcc_id"
-    user_id = "your_user_id"
-    serial = rand_serial(10) # or replace with your actual serial number
-    callsign = "YourCallSign"
-    category = "A" # your CBSD category
-    interface = "E_UTRA" # change to match your interface
+    fcc_id = "test_fcc_id_c"
+    user_id = "test_user_id_c"
+    serial = rand_serial(10)
+    callsign = "HariniNet"
+    category = "A"
+    interface = "E_UTRA"
     installation = {"latitude": 38.881405, "longitude": -77.108941, "height": 3.0,
     "heightType": "AGL", "indoorDeployment": "True", "eirpCapability": 25,
     "antennaGain": 8 }
@@ -41,7 +39,6 @@ def main():
 
     new_cbsd = CBSD()
 
-    # Send a registration request
     try:
         print('====================\n***Begin Registration Request***')
         cbsdId, responseCode, responseMessage, responseData = new_cbsd.register(fcc_id, user_id, serial, callsign, category, interface, installation)
@@ -60,10 +57,8 @@ def main():
         print(err)
         sys.exit(0)
 
-    # sleep between requests to leave enough time for the repsonse
     time.sleep(2)
 
-    # Send a spectrum inquiry
     try:
         print('====================\n***Begin Spectrum Inquiry***')
         low_freq, high_freq, responseCode, responseMessage, responseData = new_cbsd.inquiry(cbsdId)
@@ -73,7 +68,7 @@ def main():
         print("Response Code: %s" %(responseCode))
         print("Response Message: %s" %(responseMessage))
         print("Response Data: %s" %(responseData))
-        print('***End of Registration Request***')
+        print('***End of Spectrum Inquiry***')
         
         if responseCode > 0:
             raise ValueError('Response Code %s. Spectrum Inquiry failed with %s'%(responseCode, responseMessage))
@@ -84,7 +79,6 @@ def main():
 
     time.sleep(2)
 
-    # Send a grant request
     try:
         print('====================\n***Begin Grant Request***')
         grantId, grantExpireTime, heartbeatInterval, channelType, responseCode, responseMessage, responseData = new_cbsd.grant_request(cbsdId, eirp, low_freq, high_freq)
@@ -107,7 +101,6 @@ def main():
 
     time.sleep(2)
 
-    # Send the first heartbeat request
     try:
         print('====================\n***Begin Heartbeat Request #1')
         responseCode, transmitExpireTime, responseMessage, responseData = new_cbsd.heartbeat(cbsdId, grantId, 'GRANTED')
@@ -125,10 +118,8 @@ def main():
         print(err)
         sys.exit(0)
 
-    # Wait between each heartbeat request until heartbeat interval -1 second before sending the next heartbeat request
     time.sleep(heartbeatInterval-1)
     
-    # Send N - 1 heartbeat requests before relinquishing the band
     for beats in range(args.heartbeats-1):
         try:
             print('====================\n***Begin Heartbeat Request #%s***'%(beats+2))
@@ -149,7 +140,6 @@ def main():
             print(err)
             sys.exit(0)
 
-    # Send a relinquishment request
     try:
         print('====================\n***Begin Relinquishment Request***')
         responseCode, responseMessage, responseData = new_cbsd.relinquish(cbsdId, grantId)
@@ -168,7 +158,6 @@ def main():
 
     time.sleep(2)
 
-    # Send a deregistration request
     try:
         print('====================\n***Begin Deregistration Request***')
         responseCode, responseMessage, responseData = new_cbsd.deregister(cbsdId)
